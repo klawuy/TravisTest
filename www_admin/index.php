795f06b70962e83845831a27122e88ae2af95cd6
kevin lawuy <kevin lawuy>
2017-04-11 15:07:51 +0200

    Création d'une classe et d'un test unitaire bidon + modification de .travis.yml

---
+++ www_admin/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     WebQuin
+ *
+ * @version     1.0.0
+ * @copyright   Copyright (C) 2015 ETD Solutions. Tous droits réservés.
+ * @license     http://etd-solutions.com/LICENSE
+ * @author      ETD Solutions http://etd-solutions.com
+ */
+
+if (version_compare(PHP_VERSION, '7.0', '<')) {
+    die('Your host needs to use PHP 7.0 or higher to run this application.');
+}
+// Define required paths
+define('JPATH_ROOT',          dirname(__DIR__));
+define('JPATH_CONFIGURATION', JPATH_ROOT . '/App/Config');
+define('JPATH_SETUP',         JPATH_ROOT . '/App/Setup');
+define('JPATH_TEMPLATES',     JPATH_ROOT . '/App/Templates');
+
+// Load the Composer autoloader
+require JPATH_ROOT . '/vendor/autoload.php';
+
+$container = new \Joomla\DI\Container;
+$container->registerServiceProvider(new \App\Service\ConfigServiceProvider(JPATH_CONFIGURATION . '/config.json'))
+	->registerServiceProvider(new \App\Service\DatabaseServiceProvider);
+
+// Instantiate the application.
+$application = new \App\App($container);
+
+// Execute the application.
+$application->execute();
